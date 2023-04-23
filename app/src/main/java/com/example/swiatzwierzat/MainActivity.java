f78e6eac68d05cf78d7e47119cb96eba9c2ddac0
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2023-04-23 10:36:52 +0200

    AAUZ-001: Login

--- app/src/main/java/com/example/swiatzwierzat/MainActivity.java
+++ app/src/main/java/com/example/swiatzwierzat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.swiatzwierzat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +11,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.example.swiatzwierzat.configuration.BackendConfig;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializer(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
-
-        backendConfig = new BackendConfig();
 
         inEmail = findViewById(R.id.et_email);
         inPassword = findViewById(R.id.et_password);
@@ -39,27 +44,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         initializer(savedInstanceState);
 
+        Context context = this.getApplicationContext();
+
+        backendConfig.setSharedPreferences(context.getSharedPreferences(String.valueOf(R.string.preference_file_key), Context.MODE_PRIVATE));
+
         loginButton.setOnClickListener(this::onLogin);
         registerButton.setOnClickListener(this::onRegister);
         forgetButton.setOnClickListener(this::onForget);
     }
 
-    private void setCredentials(String email, String password) {
-        backendConfig.setEmail(email);
-        backendConfig.setPassword(password);
-    }
     private void onLogin(View v) {
-        setCredentials(inEmail.getText().toString(), inPassword.getText().toString());
+        AndroidNetworking.post(backendConfig.getUrl() + "/login")
+                .addBodyParameter("email", inEmail.getText().toString())
+                .addBodyParameter("password", inPassword.getText().toString())
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.w("backend.config.success", response.toString());
+                        backendConfig.setIsLogged(true);
+                        try {
+                            backendConfig.setToken("Bearer " + response.getString("access_token"));
+                            backendConfig.setRefreshToken(response.getString("refresh_token"));
+                            Toast.makeText(v.getContext(), "Correctly logged in!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(v.getContext(), ProductsActivity.class));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-        Boolean logged = backendConfig.login();
-        Log.w("swiatzwierzat.onlogin.valid", logged.toString());
-        if(logged) {
-            Toast.makeText(v.getContext(), "Correctly logged in!", Toast.LENGTH_LONG).show();
-            startActivity(new Intent(v.getContext(), ProductsActivity.class));
-
-        } else {
-            Toast.makeText(getApplicationContext(), "Credentials seems wrong! Please try again.", Toast.LENGTH_LONG).show();
-        }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.w("backend.config.error", anError.toString());
+                        Toast.makeText(getApplicationContext(), "Credentials seems wrong! Please try again.", Toast.LENGTH_LONG).show();
+                        backendConfig.setToken(null);
+                        backendConfig.setIsLogged(false);
+                    }
+                });
     }
 
     private void onRegister(View v) {
--- app/src/main/java/com/example/swiatzwierzat/configuration/BackendConfig.java
+++ app/src/main/java/com/example/swiatzwierzat/configuration/BackendConfig.java
@@ -1,100 +1,46 @@
 package com.example.swiatzwierzat.configuration;
 
 import android.content.SharedPreferences;
-import android.util.Log;
 
-import androidx.annotation.Nullable;
-
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONObjectRequestListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class BackendConfig extends Concurent {
+public class BackendConfig {
     private static String url = "https://app-web-uz.herokuapp.com/api/v1";
-    private static String host = "app-web-uz.herokuapp.com:443";
-    private static String email = "";
-    private static String password = "";
     private static String token = "";
     private static String refreshToken = "";
     private static boolean isLogged = false;
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
 
-    public static void setEmail(String email) {
-        BackendConfig.email = email;
+    public static String getToken() {
+        return token;
     }
 
-    public static void setPassword(String password) {
-        BackendConfig.password = password;
+    public static void setToken(String token) {
+        editor.putString("token", token);
+        BackendConfig.token = token;
     }
 
-    public static boolean login() {
-        AndroidNetworking.post(url + "/login")
-                .addBodyParameter("email", email)
-                .addBodyParameter("password", password)
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.w("backend.config.success", response.toString());
-                        isLogged = true;
-                        try {
-                            token = "Bearer " + response.getString("access_token");
-                            Log.w("token", token);
-                            refreshToken = response.getString("refresh_token");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
+    public static String getRefreshToken() {
+        return refreshToken;
+    }
 
-                    @Override
-                    public void onError(ANError anError) {
-                        Log.w("backend.config.error", anError.toString());
-                        token = null;
-                        isLogged = false;
-                    }
-                });
+    public static String getUrl() {
+        return url;
+    }
 
+    public static void setRefreshToken(String refreshToken) {
+        editor.putString("refreshToken", refreshToken);
+        BackendConfig.refreshToken = refreshToken;
+    }
+
+    public static void setSharedPreferences(SharedPreferences sharedPreferences) {
+        BackendConfig.sharedPreferences = sharedPreferences;
+    }
+
+    public static boolean isIsLogged() {
         return isLogged;
     }
 
-    public static boolean isValid() {
-        Log.w("backend.config.valid.token", token.toString());
-        AndroidNetworking.get(url + "/users")
-                .addHeaders("Authorization", token)
-                .addHeaders("Host", host)
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.w("backend.config.valid.success", response.toString());
-                        isLogged = true;
-                    }
-
-                    @Override
-                    public void onError(ANError anError) {
-                        Log.w("backend.config.valid.error", anError.getErrorBody());
-                        isLogged = false;
-                    }
-                });
-
-        return isLogged;
+    public static void setIsLogged(boolean isLogged) {
+        BackendConfig.isLogged = isLogged;
     }
-
-
 }
