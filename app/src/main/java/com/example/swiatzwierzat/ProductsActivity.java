1a19bce5c174b0ee419a194ccc62d35fc1932ad4
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2023-05-14 20:56:16 +0200

    Firebase configuration

--- app/src/main/java/com/example/swiatzwierzat/ProductsActivity.java
+++ app/src/main/java/com/example/swiatzwierzat/ProductsActivity.java
@@ -1,13 +1,56 @@
 package com.example.swiatzwierzat;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.swiatzwierzat.configuration.BackendConfig;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ProductsActivity extends AppCompatActivity {
+
+    private GridView gridView;
+
+    private BackendConfig backendConfig;
+    private void initializer(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_products);
+
+        gridView = findViewById(R.id.gv_products_products);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("com.example.swiatzwierzat.firebase", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("com.example.swiatzwierzat.firebase", "Failed to read value.", error.toException());
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        initializer(savedInstanceState);
     }
+
+
 }
